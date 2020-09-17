3820b3fd5e4cf64fcea1df6e6c4e1107254a0551
megfernu <megfernu@gmail.com>
2020-09-17 22:47:25 +0100

    fim de estrutura de codigo

--- login.py
+++ login.py
@@ -1,12 +1,14 @@
 import csv
 
-with open('logs.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
-
-    
-
-
-users = {'nome:','nuno'}
+def menu():
+    opcao = input('1 - Login\n2 - Registar\n>>>')
+    if opcao == '1':
+        login()
+    elif opcao == '2':
+        register()
+    else:
+        print('Escolha uma opção válida.')
+        menu()
 
 
 
@@ -15,56 +17,47 @@ def login():
         csv_reader = csv.reader(csv_file)
 
         next(csv_reader)
-
-    #name = users.get(input('Utilizador:'))
-        name = input('utilizador:')
-        senha = input('senha:')
+        nome = input('nome:')
+        temp = False
+        
         for line in csv_reader:
-             if name == line[0]:
-                print('a trabalhar')
-                #if senha == line[1]:
-                    #print('Pode entrar.')
-                #else:
-                    #print('senha errada')
-             else:
-                 print('nome de utilizador não registado')
-                 #menu()
-            
-   # if name != None:
-        #senha = input('Password:')
-       # if senha == name:
-        #    print('Pode entrar.')
-    #else:
-        #print('Nome de utilizador não registado.')
-       # menu()
+             if nome == line[0]:
+                temp = True
+                print('muito bom')
+                senha = input('senha:')
+                if senha == line[1]:
+                    print('pass correta')
+                else:
+                    print('Pass errada')
+                    menu()
+        if temp == False:
+            print('Utilizador nao regitado')
+            menu()
+               
 
 def register():
-    registo = input('Introduza o nome de utilizador:')
-    if users.get(registo) != None:
-        print('Utilizador ja existe.')
-        register()
-    else:
-        users[registo] = ''
-        valor = input('Introduza a password:')
-        valor2 = input('Repita a password:')
-        if valor == valor2:
-            users[registo] = valor
-            print('Registado com sucesso')
-            print(users)
-        else:
-            print('A password não é igual.')
-            register()
+    with open('logs.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
 
-def menu():
-    escolha = input('1 - Login\n2 - Register\n>>:')
-    if escolha == '1':
-        login()
-    elif escolha == '2':
-        register()
-    else:
-        print('Introduza uma opção correta.')
-        menu()
+        next(csv_reader)
+        registo = input('Introduza o nome de utilizador:')
+        for line in csv_reader:
+            if registo == line[0]:
+                print('utilizador ja registado')
+                register()
+                
+
+        senha = input('Introduza a password:')
+        lista = [registo, senha]
+        with open(r'logs.csv', 'a') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(lista)
+            csv_file.close()
+            menu()
 
 
 
 menu()
+
+
+
